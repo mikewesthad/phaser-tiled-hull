c3b38c124a2a833aa8a5ba956baece443576a221
Michael Hadley <michaelwesthadley@gmail.com>
2017-06-11 10:22:07 -0500

    Reset

--- src/library/index.js
+++
@@ -1,3 +0,0 @@
-// Fix for webpack not exporting ES6 module default properly when using global mode of UMD. It ends
-// up with something having to do "phaserTiledHull.default(...)" instead of "phaserTiledHull(...)"
-module.exports = require("./tiled-hull").default;
